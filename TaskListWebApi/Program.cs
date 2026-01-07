7b83cd237f57e8b30b24fd5be67ffd4a3121a295
Mike Jones <mikiej_boy@hotmail.com>
2026-01-07 21:56:10 +0000

    Add JWT authentication and custom exception handling
    Implemented JWT authentication using JwtBearer, added related NuGet packages, and configured token validation in Program.cs. Introduced BaseException and UserNotFoundException for improved error handling. Added BaseAuthController for authorized endpoints and AuthInitiateController for login/register actions. Centralized JWT route constants in JwtMiddlewareRoutes. Updated project file with new dependencies and folder structure.

--- TaskListWebApi/Program.cs
+++ TaskListWebApi/Program.cs
@@ -1,11 +1,12 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
+using System.Text;
 using TaskList.Api.Application;
 using TaskList.Api.Domain.Users.Models.AuthenticationModels;
 using TaskList.Api.Infrastructure;
 using TaskList.Api.Infrastructure.Data;
-using TaskList.Api.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +22,30 @@ builder.Services.AddOpenApi();
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<TaskListDbContext>()
     .AddSignInManager();
+
+
+var key = builder.Configuration.GetSection("JWT:Key").Value!;
+var issuer = builder.Configuration.GetSection("JWT:Issuer").Value!;
+var audience = builder.Configuration.GetSection("JWT:Audience").Value!;
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = false,
+        ValidateActor = false,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience
+    };
+});
 
 var app = builder.Build();
 
